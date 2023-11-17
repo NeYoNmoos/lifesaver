4371e60ef13a7b6687f3bc31420e78646d8da319
Matthias <neyon@gmx.at>
2023-11-17 17:13:43 +0100

    made getting data possible

--- scripts/background.js
+++ scripts/background.js
@@ -5,8 +5,6 @@ let domainTimes = {};
 function getDomain(url) {
   try {
     const hostname = new URL(url).hostname;
-    // Extract the domain. This will include subdomains.
-    // Adjust as needed (e.g., removing 'www').
     return hostname;
   } catch (e) {
     console.error("Error parsing URL:", e);
@@ -14,20 +12,25 @@ function getDomain(url) {
   }
 }
 
+function updateTimeSpent() {
+  if (activeDomain && startTime) {
+    const timeSpent = Date.now() - startTime;
+    domainTimes[activeDomain] = (domainTimes[activeDomain] || 0) + timeSpent;
+
+    chrome.storage.local.set({ domainTimes }, function () {
+      console.log(
+        `Time updated for ${activeDomain}: ${domainTimes[activeDomain]}`
+      );
+    });
+  }
+}
+
 chrome.tabs.onActivated.addListener((activeInfo) => {
   chrome.tabs.get(activeInfo.tabId, (tab) => {
-    console.log("trying to get tab data!");
     if (tab.url) {
       const domain = getDomain(tab.url);
-
       if (domain) {
-        if (activeDomain && startTime) {
-          const timeSpent = Date.now() - startTime;
-          domainTimes[activeDomain] =
-            (domainTimes[activeDomain] || 0) + timeSpent;
-          chrome.storage.local.set({ domainTimes });
-        }
-
+        updateTimeSpent(); // Update time spent on the previous domain
         startTime = Date.now();
         activeDomain = domain;
       }
@@ -36,20 +39,18 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // Additional listener to handle URL changes in the same tab
   if (tab.active && changeInfo.url) {
     const domain = getDomain(changeInfo.url);
-
     if (domain && domain !== activeDomain) {
-      if (activeDomain && startTime) {
-        const timeSpent = Date.now() - startTime;
-        domainTimes[activeDomain] =
-          (domainTimes[activeDomain] || 0) + timeSpent;
-        chrome.storage.local.set({ domainTimes });
-      }
-
+      updateTimeSpent(); // Update time spent on the previous domain
       startTime = Date.now();
       activeDomain = domain;
     }
   }
+});
+
+chrome.tabs.onRemoved.addListener(() => {
+  updateTimeSpent(); // Update time when a tab is closed
+  startTime = undefined;
+  activeDomain = undefined;
 });
