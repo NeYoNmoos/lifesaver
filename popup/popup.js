c98d7ee8476181764e97f101e733df53d55f1487
it-is-chr1s <Andreko@gmx.de>
2023-11-18 17:47:12 +0100

    added flo stuff, not working yet

--- popup/popup.js
+++ popup/popup.js
@@ -61,8 +61,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
   
   loadEvent().then((events) => {
-    events.forEach((event) => {
-      console.log(event)
+    for(let indexEvent = 0; indexEvent < events.length; indexEvent++){
+      console.log(events[indexEvent])
       var newText = document.createTextNode('This is a new paragraph.');
 
       const child = `<style>
@@ -79,10 +79,12 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     </style>
     <div class="page-component">
-      <p class="text-gray-600">${event.title}</p>
+      <p class="text-gray-600">${events[indexEvent].title}</p>
       <div class="flex space-x-4 items-center mt-4 edit-container">
-        <img class="w-16 h-16 border" id="imageSource" src="${event.icon}">
-        <canvas class="w-16 h-16 border"></canvas>
+        <img class="w-16 h-16 border" id="imageSource" src="${events[indexEvent].icon}">
+        <div class="chart-container">
+          <canvas id="${"myChart" + indexEvent}"></canvas>
+        </div>
         <input class="w-24 px-2 py-1 border" style="display: none;">
         <button class="bg-blue-500 text-white px-4 py-2 rounded">
           <i class="fas fa-pen"></i> Edit
@@ -93,8 +95,79 @@ document.addEventListener('DOMContentLoaded', function() {
       </div>
     </div>`
       summary.insertAdjacentHTML('beforeend', child);
-    });
+      drawGraph("myChart" + indexEvent,'h',10,20);
+    }
   });
+
+  const chartJS = document.createElement('script');
+  chartJS.src = 'https://cdn.jsdelivr.net/npm/chart.js@3.3.2/dist/chart.min.js';
+  
+  function drawGraph(chartID, timeformat, timeleft, timeonline){
+    chartJS.onload = () => {
+      const branches_canvas = document.getElementById(chartID);
+
+      const branches = new Chart(branches_canvas, {
+          type: 'bar',
+          data: {
+              labels: [''],
+              datasets: [{
+                  label: 'Time Spent',
+                  data: [timeonline],
+                  backgroundColor:[ 
+                      '#ca2e2e',
+                  ],
+                  hoverOffset: 0
+              },
+              {
+                  label: 'Time Left',
+                  data: [timeleft],
+                  backgroundColor: [
+                      '#d16f6f',
+                  ],
+              }],
+          },
+          options: {
+              indexAxis: 'y',
+              plugins: {
+                  legend: {
+                      display: false // Hide the legend
+                  },
+                  tooltip: {
+                      callbacks: {
+                          label: function(context) {
+                              let label = context.dataset.label || '';
+                              if (label) {
+                                  label += ': ';
+                              }
+                              if (context.parsed.y !== null) {
+                                  label += context.parsed.x + timeformat;
+                              }
+                              return label;
+                          },
+                          footer: function(tooltipItems, data) {
+                              let sum = 0;
+                              tooltipItems.forEach(function(tooltipItem) {
+                                  sum += tooltipItem.parsed.x; // Access 'y' value directly for the 'y' axis
+                              });
+                              return 'Total: ' + sum + ' ' + timeformat;
+                          }
+                      }
+                  },
+              },
+              scales: {
+                  x: {
+                      display: true,
+                      stacked: true
+                  },
+                  y: {
+                      display: false,
+                      stacked: true
+                  }
+              },
+          }
+      });
+  };
+  }
 
   loadEvent();
 });
